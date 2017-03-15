03649a9a969004e4fd25d475d843bcec85e4808d
Pavel Krivanek <pavel.krivanek@gmail.com>
2017-03-15 15:46:44 +0100

    symlinks relativity

--- scripts/createAllSymlinks.sh
+++ scripts/createAllSymlinks.sh
@@ -1,4 +1,4 @@
-
-find ../src -type d -name "*.package" -print0 | while IFS= read -r -d $'\0' line; do
-  ln -s "$line" "../src/all/$(basename $line)"
+cd ../src/all
+find .. -type d -name "*.package" -print0 | while IFS= read -r -d $'\0' line; do
+  ln -s "$line" "$(basename $line)"
 done
