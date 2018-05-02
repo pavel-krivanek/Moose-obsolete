89d371b00c71fbdadd8937a93cbb452c84fead26
Pavel Krivanek <pavel.krivanek@gmail.com>
2018-05-02 10:24:58 +0200

    scripts

---
+++ scripts/buildImage.sh
@@ -0,0 +1,17 @@
+set -e
+
+wget --quiet -O - http://get.pharo.org/70+vm | bash
+
+IMAGE_NAME=Pharo
+
+if [ $# -eq 0 ]
+  then
+    echo "No image name supplied"
+  else
+    IMAGE_NAME="$1"
+    echo "Image name: $IMAGE_NAME"
+    ./pharo Pharo.image save $IMAGE_NAME
+fi
+
+/pharo $IMAGE_NAME.image eval --save  "Metacello new baseline: 'Moose'; repository: 'tonel://./Moose/src/all'; load."
+
